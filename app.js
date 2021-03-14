120c07f8e1ef269dd862706d7854aea2a27c6eca
Michael Eisenbraun <=>
2021-03-14 14:49:23 -0400

    Adds final version

--- app.js
+++ app.js
@@ -5,7 +5,44 @@ function randomNumber (min, max) {
 const app = Vue.createApp({
   data: function () {
     return {
-      show: false
+      score: 0,
+      bubbles: 0,
+      active: false,
+      level: 'medium'
+    }
+  },
+  methods: {
+    addPoint: function () {
+      this.score++
+    },
+    bubblePopped: function () {
+      this.bubbles--
+      if (!this.bubbles) {
+        const that = this
+        setTimeout(function () {
+          that.active = false
+        }, 1000)
+      }
+    },
+    play: function (level) {
+      this.level = level
+      this.score = 0
+      this.bubbles = 20
+      this.active = true
+    }
+  }
+})
+
+app.component('bubble', {
+  props: ['level'],
+  data: function () {
+    return {
+      show: false,
+      levels: {
+        easy: 10,
+        medium: 6,
+        hard: 4
+      }
     }
   },
   mounted: function () {
@@ -25,12 +62,20 @@ const app = Vue.createApp({
 
       setTimeout(function () {
         that.show = false
-      }, randomNumber(2, 4) * 1000)
+        that.$emit('bubble-popped')
+      }, randomNumber(that.levels[that.level] - 2, that.levels[that.level]) * 1000)
     },
     pop: function () {
       this.show = false
+      this.$emit('add-point')
     }
-  }
+  },
+  template: `
+    <div class="bubble-container">
+      <transition enter-active-class="animate__animated animate__bounceIn">
+        <div class="bubble" v-if="show" @click.once="pop"></div>
+      </transition>
+    </div>`
 })
 
 const vm = app.mount('#app')
